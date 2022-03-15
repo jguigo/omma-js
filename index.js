96b30fe545262c65e1a2f9d189011818521e75c5
Guilherme Melo <jguilhermefmelo21@gmail.com>
2022-03-15 19:46:17 -0300

    um arquivo

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 const NOME_DA_EMPRESA = " ---------------> SISTEMA OMMA <---------------";
 console.log(NOME_DA_EMPRESA);
-console.log('-----------------------------------------------\n');
+console.log("-----------------------------------------------\n");
 const receitas = [];
 let id = 1;
 
-//Fazer cadastro da receita por um "input" 
-function cadastrarReceita(
+//Fazer cadastro da receita por um "input"
+const cadastrarReceita = (
    nome,
    dificuldade,
    ingredientes,
    preparo,
    link,
    vegan
-) {
+) => {
    receitas.push({
       id,
       nome,
@@ -24,39 +24,71 @@ function cadastrarReceita(
    });
    id++;
    console.log("Cadastro concluído\n");
-   console.log('-----------------------------------------------\n')
-}
-
+   console.log("-----------------------------------------------\n");
+};
 
 //Exibir receitas
-function exibirReceitas(){
-    for(let i = 0; i < receitas.length; i++){
-        console.log(`Receita: ${receitas[i].nome}\n`);
-        console.log(`Ingredientes: `)
-        for(let j = 0; j<receitas[i].ingredientes.length; j++){
-            console.log(`-> ${receitas[i].ingredientes[j]}`);
-        }
-        console.log(`\n Vegan: ${receitas[i].vegan}`)
-        console.log('-----------------------------------------------\n')
-    }
-}
+const exibirReceitas = () => {
+   receitas.forEach((e) => {
+      console.log(`Receita: ${e.nome}\n`);
+      console.log("Ingredientes:");
+      e.ingredientes.forEach((item) => console.log(`-> ${item}`));
+      console.log(`\n'Vegan: ${e.vegan}`);
+      console.log("\n-----------------------------------------------\n");
+   });
+};
 
 //deletar um item pelo id
-function deletarReceita(id){
-    for(let i = 0; i < receitas.length; i++){
-        if(id === receitas[i].id){
-            receitas.splice(i, 1);
-            return console.log('Receita deletada com sucesso!');
-        }
-    }
-
-    console.log('Receita não encontrada!');
+const deletarReceita = (id) => {
+   let verificador = 0;
+   receitas.forEach((e) => {
+      if (id === e.id) {
+         receitas.splice(receitas.indexOf(e), 1);
+         verificador++;
+      }
+   })
+   if(verificador>0){
+      return console.log("Receita deletada com sucesso!\n\n"); 
+   }
+   console.log('Receita não encontrada!\n\n');
 }
+
+//buscar uma receita
+const buscarReceita = termo => {
+   console.log(`Palavra chave: "${termo}"`)
+   const busca = receitas.filter(e => e.nome.includes(termo));
+   busca.forEach(e => console.log(e.nome));
+}
+
+//atualizar receita
+const atualizarReceita = (id, nome, dificuldade, ingredientes, preparo, link, vegan) => {
+   let verificador = 0;
+   receitas.forEach(e => {
+      if(e.id === id){
+         e.nome = nome;
+         e.dificuldade = dificuldade;
+         e.ingredientes = ingredientes;
+         e.preparo = preparo;
+         e.link = link;
+         e.vegan = vegan;
+         verificador++;
+      }
+   });
+   if(verificador>0){
+      return console.log(`Receita ${id} atualizada!\n\n`); 
+   }
+   console.log(`ID não encontrado!\n\n`);
+}
+
 
 cadastrarReceita(
    "Risoto de Abóbora",
    "modereda",
-   ["1/2 kg de abóbora tipo kabotchá", "2 xícaras (chá) de arroz arbóreo", "2 colheres (sopa) de manteiga sem sal"],
+   [
+      "1/2 kg de abóbora tipo kabotchá",
+      "2 xícaras (chá) de arroz arbóreo",
+      "2 colheres (sopa) de manteiga sem sal",
+   ],
    "Coloca agua na panela, e joga tudo lá dentro",
    "https://www.youtube.com/watch?v=Mxc3RQE4yAg&feature=emb_title",
    true
@@ -69,6 +101,3 @@ cadastrarReceita(
    "youtube.com.br",
    false
 );
-
-exibirReceitas();
-
